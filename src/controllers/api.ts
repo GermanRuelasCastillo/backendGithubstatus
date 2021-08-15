aec3dbd2313c9ebd3981689db67eb6c1e6ca0848
GermanRuelas <german.ruelas@tigears.com>
2021-08-14 19:42:24 -0500

    installing axios
    install axios for github request

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,9 +1,24 @@
 import { Application, Request, Response } from "express";
+const axios = require('axios')
 
-import CoursesData from "../../data/courses.json";
+let  _url = 'https://api.github.com/';
 
 export const loadApiEndpoints = (app: Application): void => {
-  app.get("/api", (req: Request, res: Response) => {
-    return res.status(200).send(CoursesData);
+  app.get("/api/getRepos", async (req: Request, res: Response) => {
+    try {
+      const response = await axios.get(_url+'users/GermanRuelasCastillo/repos')
+      return res.status(200).json(response.data);
+    } catch (error) {
+      return res.status(404).json(error);
+    }
+  });
+
+  app.get("/api/getCommits/:project/:branch", async (req: Request, res: Response) => {
+    try {
+      const response = await axios.get(_url+'repos/GermanRuelasCastillo/'+req.params.project+'/commits?sha='+req.params.branch);
+      return res.status(200).json(response.data);
+    } catch (error) {
+      return res.status(404).json(error);
+    }
   });
 };
